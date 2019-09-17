e2c14ace539e3a6d7ecf934af6223a5b7dbbb6ee
Larissa Oliveira <lari.mello91@gmail.com>
2019-09-17 09:56:44 -0300

    HelloWorld Exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { timingSafeEqual } from 'crypto';
 
 /** 
  * @author Larissa Oliveira
@@ -15,10 +16,22 @@ export class AppComponent {
   title = 'Hello World from WMAD Class 2020';
   name = 'Larissa';
   courseName = 'Alt Web - Angular';
+  myCollection=[3, 4, 5, 6, 7, 8, 9, 10];
+  count = '';
+  students=['Camille', 'Kevin', 'Roman'];
 
   constructor(){
     this.courseName = "Angular 8";
     console.log(`This is my component's TS file. My name is ${this.name}`);
+    this.loopCount();
+  }
+
+  loopCount() {
+    for(let i = 0; i < this.myCollection.length; i++) {
+      this.count += this.myCollection[i] + ' ';
+    }
+
+    this.myCollection.forEach(n  => {this.count = this.count + ' ' + n});
   }
 }
 
